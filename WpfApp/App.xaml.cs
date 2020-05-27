76c8134109663fd377d72aa55e7a72ac8cc4e72d
Crow Scare <ciki3100@gmail.com>
2020-05-27 02:05:02 +0200

    ed

---
+++ WpfApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    //het ontwerp van de schermen is vrij te kiezen 
+    //    maar de user stories uit de paragraaf ‘context’ moeten wel uitvoerbaar zijn]
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
