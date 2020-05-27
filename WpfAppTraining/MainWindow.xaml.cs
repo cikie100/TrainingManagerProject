9b5cd55eeff3ee15f16834c7e6d1ee9e4d9a8879
Crow Scare <ciki3100@gmail.com>
2020-05-27 06:51:12 +0200

    edd

--- WpfAppTraining/MainWindow.xaml.cs
+++ WpfAppTraining/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfAppTraining
 {
@@ -23,20 +13,21 @@ namespace WpfAppTraining
     public partial class MainWindow : Window
     {
         trainingDBEntities dataEntities = new trainingDBEntities();
+        public ObservableCollection<Grid> GridCollection { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
+            GridCollection = new ObservableCollection<Grid>();
 
             //Voor Cycling
             List<CyclingSession> cyclings = new List<CyclingSession>();
 
             foreach (CyclingSession sess in dataEntities.CyclingSessions)
             {
-                cyclings.Add(sess);
+                dataGrid1.Items.Add(sess);
             }
 
-            dataGrid1.ItemsSource = cyclings.ToList();
 
             ///////////////////////////////////////////////////
             //Voor Running
@@ -49,5 +40,43 @@ namespace WpfAppTraining
             dataGrid2.ItemsSource = runings.ToList();
         }
 
+
+        private void btnAddCycle_Click(object sender, RoutedEventArgs e)
+        {
+            
+            foreach (CyclingSession cs in dataGrid1.Items)
+            {
+
+               // CyclingSession x = new CyclingSession(cs.ToString().);
+                if (! dataEntities.CyclingSessions.Contains(cs)){
+                 dataEntities.CyclingSessions.Add(cs);
+                }
+            }
+        }
+
+        private void btnDeleteCycle_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid1.SelectedItem != null)
+                dataEntities.CyclingSessions.Remove(dataGrid1.SelectedItem as CyclingSession);
+        }
+
+        private void btnAddRun_Click(object sender, RoutedEventArgs e)
+        {
+        //    foreach (RunningSession item in dataGrid2.Items)
+        //    {
+        //        if (!dataEntities.RunningSessions.Any(p => p.Id == item.Id)) { 
+        //        dataEntities.RunningSessions.Add(new RunningSession
+        //            (item.When,item.Distance,item.Time,item.AverageSpeed,item.TrainingType,item.co)
+        //            );
+        //        }
+        //    }
+
+        }
+
+        private void btnDeleteRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid2.SelectedItem != null)
+                dataEntities.RunningSessions.Remove(dataGrid2.SelectedItem as RunningSession);
+        }
     }
 }
