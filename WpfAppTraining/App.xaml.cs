ef6255369060d5f1e5b9b244c49fec21d2449090
Crow Scare <ciki3100@gmail.com>
2020-05-27 03:24:48 +0200

    edd

--- WpfAppTraining/App.xaml.cs
+++ WpfAppTraining/App.xaml.cs
@@ -6,11 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfApp
+namespace WpfAppTraining
 {
     /// <summary>
-    //het ontwerp van de schermen is vrij te kiezen 
-    //    maar de user stories uit de paragraaf ‘context’ moeten wel uitvoerbaar zijn]
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
---
+++ WpfAppTraining/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTraining
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        trainingDBEntities dataEntities = new trainingDBEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Voor Cycling
+            List<CyclingSession> cyclings = new List<CyclingSession>();
+
+            foreach (CyclingSession sess in dataEntities.CyclingSessions)
+            {
+                cyclings.Add(sess);
+            }
+
+            dataGrid1.ItemsSource = cyclings.ToList();
+
+            ///////////////////////////////////////////////////
+            //Voor Running
+            List<RunningSession> runings = new List<RunningSession>();
+
+            foreach (RunningSession rsess in dataEntities.RunningSessions)
+            {
+                runings.Add(rsess);
+            }
+            dataGrid2.ItemsSource = runings.ToList();
+        }
+
+    }
+}
